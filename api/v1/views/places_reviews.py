05f73edebe80dafaba2362400dcc0312f109ca32
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-27 11:56:04 -0700

    Added new module

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from models import storage
 from models.base_model import BaseModel
 from models.review import Review
+from models.place import Place
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
