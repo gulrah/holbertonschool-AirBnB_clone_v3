024c9475f112c6fdfc9445b7edc4415e8c1d1096
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-27 12:35:12 -0700

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
