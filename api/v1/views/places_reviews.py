02380a440dbc366ef590c9bfb467d5df1cb960ec
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-27 12:38:19 -0700

    Update places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -17,12 +17,14 @@ def get_reviews(place_id):
     reviews = [review.to_dict() for review in place.reviews]
     return jsonify(reviews)
 
+
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
 def get_review(review_id):
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
     return jsonify(review.to_dict())
+
 
 @app_views.route('/reviews/<review_id>', methods=['DELETE'], strict_slashes=False)
 def delete_review(review_id):
@@ -33,13 +35,14 @@ def delete_review(review_id):
     storage.save()
     return jsonify({}), 200
 
+
 @app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
 def create_review(place_id):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    if not request.json:
-        abort(400, 'Not a JSON')
+    if not request.is_json:
+        abort(400, 'Invalid JSON')
     data = request.get_json()
     if 'user_id' not in data:
         abort(400, 'Missing user_id')
@@ -48,20 +51,21 @@ def create_review(place_id):
     user_id = data['user_id']
     user = storage.get(User, user_id)
     if not user:
-        abort(404)
+        abort(404, 'User not found')
     data['place_id'] = place_id
     review = Review(**data)
     storage.new(review)
     storage.save()
     return jsonify(review.to_dict()), 201
 
+
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def update_review(review_id):
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
-    if not request.json:
-        abort(400, 'Not a JSON')
+    if not request.is_json:
+        abort(400, 'Invalid JSON')
     data = request.get_json()
     ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
     for key, value in data.items():
