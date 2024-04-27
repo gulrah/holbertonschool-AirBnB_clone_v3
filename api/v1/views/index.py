dbaa4b9bc7ee077f31ed711473a1a262988357b6
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-27 11:13:07 -0700

    Missing imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 """Module"""
 
-from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
+    """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
+    """Returns the count of objects for each model"""
     counts = {
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
