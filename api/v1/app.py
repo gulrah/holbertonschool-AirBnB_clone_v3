d41f553a3254e36d670edf1de60ebafda8640c2e
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-27 12:35:56 -0700

    PEP8

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ app.register_blueprint(app_views)
 
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """Close storage connection"""
