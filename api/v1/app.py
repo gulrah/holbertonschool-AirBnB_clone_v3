885763c4416061212d63cbb7d6e19dc5846e1176
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-27 02:20:22 -0700

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,32 @@
-#!/usr/bin/python3
-"""Script that starts a Flask app"""
-from flask import Flask, jsonify
-from flask_cors import CORS
+#!/usr/bin/python3'
+""" App Module """
+
+
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, abort
 from os import getenv
+from flask_cors import CORS
 
-
-"""Start Flask"""
 app = Flask(__name__)
-
-"""Register the blueprint app_views"""
 app.register_blueprint(app_views)
+app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-"""Create the CORS instance to allow IPs"""
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-
-@app.teardown_appcontext
-def teardown(exception):
-    """Closes session"""
-    storage.close()
-    
-    
 @app.errorhandler(404)
-def errorhandler(error):
-    """Returns a JSON-formated status code for errors"""
+def not_found(e):
+    """ Returns a 404 error in JSON format """
     return jsonify({"error": "Not found"}), 404
 
 
+@app.teardown_appcontext
+def teardown(close):
+    ''' Closes current storage session '''
+    storage.close()
+
+
 if __name__ == "__main__":
-    API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
-    API_PORT = getenv("HBNB_API_PORT", 5000)
-    app.run(host=API_HOST, port=API_PORT, threaded=True)
+    hosts = getenv("HBNB_API_HOST", default='0.0.0.0')
+    ports = int(getenv("HBNB_API_PORT", default=5000))
+    app.run(host=hosts, port=ports, threaded=True)
