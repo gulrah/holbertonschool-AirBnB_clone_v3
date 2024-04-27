be0742637d24d06ed78196adcb0f64e57999e3c7
gulrah <gulnarrahimli@mail.ru>
2024-04-27 13:13:18 +0000

    Implemented endpoints

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,15 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def api_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(counts)
